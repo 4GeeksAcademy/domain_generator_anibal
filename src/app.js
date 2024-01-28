5fa5e5340a6370a3a7dc0fe57a77773f98a618c9
Anibal-JPG <109487916+Anibal-JPG@users.noreply.github.com>
2024-01-28 23:04:43 +0000

    intento 1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = ".com";
+  let namesdoamins = [];
+
+  let nameDomainGenerator = (pronombre, adjetivo, sustantivo) => {
+    for (let pronombre = 0; pronombre < pronoun.length; pronombre++) {
+      for (let adjetivo = 0; adjetivo < adj.length; adjetivo++) {
+        for (let sustantivo = 0; sustantivo < noun.length; sustantivo++) {
+          namesdoamins.push(
+            pronoun[pronombre] + adj[adjetivo] + noun[sustantivo] + extension
+          );
+        }
+      }
+    }
+
+    return namesdoamins;
+  };
+
+  nameDomainGenerator(pronoun, adj, noun);
+
+  console.log(namesdoamins);
 };
